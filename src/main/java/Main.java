22f7bc45a358c0c21b88d72b8fe3d687c214afe2
Juan Martin Restrepo <tinto922@gmail.com>
2022-07-20 12:59:54 -0400

    completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static int sumOfDivisors(int start, int end, int a, int b) {
-        // your code here
-        return 0;
+        return IntStream.rangeClosed(start, end).filter(i -> (i % a == 0) || (i % b == 0)).distinct().sum();
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the starting range: ");
         int start = scanner.nextInt();
+        System.out.print("Enter the ending range: ");
         int end = scanner.nextInt();
+        System.out.print("Enter the first divisor: ");
         int a = scanner.nextInt();
+        System.out.print("Enter the second divisor: ");
         int b = scanner.nextInt();
 
-        System.out.println(sumOfDivisors(start, end, a, b));
+        System.out.println("Result: " + sumOfDivisors(start, end, a, b));
 
         scanner.close();
     }
